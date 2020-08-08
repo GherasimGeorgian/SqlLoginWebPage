2051af0f56c40a442d306e47d1eb2a2146c39f4e
Geo <Geo@DESKTOP-4HDFJN5>
2020-08-08 22:01:05 +0300

    Add project files.

---
+++ SqlLoginWebPage/Login.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+namespace SqlLoginWebPage
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblErrorMessage.Visible = false;
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = |DataDirectory|\LoginDB.mdf; Integrated Security = True; Connect Timeout = 30")) {
+                sqlcon.Open();
+                string query = "SELECT COUNT(1) FROM tblUser WHERE username=@username and password=@password";
+                SqlCommand sqlcmd = new SqlCommand(query, sqlcon);
+                sqlcmd.Parameters.AddWithValue("@username",txtUserName.Text.Trim());
+                sqlcmd.Parameters.AddWithValue("@password",txtPassword.Text.Trim());
+                int count = Convert.ToInt32(sqlcmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    Session["UserName"] = txtUserName.Text.Trim();
+                    Response.Redirect("Dashboard.aspx");
+                }
+                else
+                {
+                    lblErrorMessage.Visible = true;
+                }
+            }
+        }
+    }
+}
