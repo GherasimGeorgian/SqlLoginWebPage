08264e54951f027d912b43b25c88c96f1c3a3a3d
Geo <Geo@DESKTOP-4HDFJN5>
2020-08-08 22:19:36 +0300

    LogOut option

---
+++ SqlLoginWebPage/Dashboard.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Dashboard.aspx.cs" Inherits="SqlLoginWebPage.Dashboard" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblUserDetails" runat="server" Text="Label"></asp:Label>
+            <br/>
+            <asp:Button ID="bntLogout" runat="server" Text="LogOut" OnClick="bntLogout_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ SqlLoginWebPage/Dashboard.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SqlLoginWebPage
+{
+    public partial class Dashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserName"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            lblUserDetails.Text = "UserName: " + Session["UserName"];
+        }
+
+        protected void bntLogout_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
